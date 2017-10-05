466ff1f29d2c710c81e407b3a8f9f01f34130a44
marie <fshbon677@gmail.com>
2017-10-05 04:30:46 +0000

    Made 1 function and print day only once

--- produce_summary.py
+++ produce_summary.py
@@ -1,19 +1,17 @@
 def print_delivery_report(day_number, data_file_by_day):
     the_file = open(data_file_by_day)
+    print "Day {}".format(day_number)
     for line in the_file:
         line = line.rstrip()
         words = line.split('|')
 
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
 
+        print "Delivered {} {}s for total of ${}".format(count, melon, amount)
+        
     the_file.close()
-
-    print "Day {}".format(day_number)
-    print "Delivered {} {}s for total of ${}".format(count, melon, amount)
-    print
-
 
 print_delivery_report(1, "um-deliveries-20140519.txt")
 
